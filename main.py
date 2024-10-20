c0653c11861ee362ec72d75dd1183be6b9899b99
Jialiang Fan <jfan5@nd.edu>
2024-10-20 16:16:37 -0400

    update locally

--- main.py
+++ main.py
@@ -110,7 +110,12 @@ def train(num_epochs=50, data_augmentation=False, batch_size=8, model_type=0, pr
             labels2 = inputs['label2'].to(device)
             
             optimizer.zero_grad()
-            outputs1, outputs2 = model(images)
+            if model_type == 16:
+                output = model(images)
+                outputs1 = output.logits[0]
+                outputs2 = output.logits[1]
+                # outputs1 = outputs1.logits
+                # outputs2 = outputs2
             loss1 = criterion1(outputs1, labels1)
             loss2 = criterion2(outputs2, labels2)
             loss = loss1 + loss2
