3b108a08d174f1e930e059c56ef981d93307cf0c
Jialiang Fan <jfan5@nd.edu>
2024-10-20 16:50:53 -0400

    update locally

---
+++ play.py
@@ -0,0 +1,16 @@
+from transformers import AutoFeatureExtractor, CvtForImageClassification
+from PIL import Image
+import requests
+
+url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+image = Image.open(requests.get(url, stream=True).raw)
+
+feature_extractor = AutoFeatureExtractor.from_pretrained('microsoft/cvt-21-384-22k')
+model = CvtForImageClassification.from_pretrained('microsoft/cvt-21-384-22k')
+
+inputs = feature_extractor(images=image, return_tensors="pt")
+outputs = model(**inputs)
+logits = outputs.logits
+# model predicts one of the 1000 ImageNet classes
+predicted_class_idx = logits.argmax(-1).item()
+print("Predicted class:", model.config.id2label[predicted_class_idx])
